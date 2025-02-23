f9ac416be9b4d8965165bd4a4de63a4e0ca9d1f3
Jey-Vaninah <hei.vaninah@gmail.com>
2025-02-23 22:16:23 +0000

    feat: get ingredient price from price history

---
+++ src/repository/Criteria.java
@@ -0,0 +1,19 @@
+package repository;
+
+public class Criteria {
+    private final String column;
+    private final Object value;
+
+    public Object getValue() {
+        return value;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public Criteria(String column, Object value) {
+        this.column = column;
+        this.value = value;
+    }
+}
