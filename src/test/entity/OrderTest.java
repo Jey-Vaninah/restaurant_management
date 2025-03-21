95586b593d8b0f9af53c9e00fe890f1c3026cb15
Jey-Vaninah <hei.vaninah@gmail.com>
2025-03-21 21:44:16 +0300

    test: add more test for order

--- src/test/entity/OrderTest.java
+++ src/test/entity/OrderTest.java
@@ -1,12 +1,17 @@
 package test.entity;
 
 import entity.Order;
+import entity.OrderStatus;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDateTime;
+import java.util.List;
 
+import static entity.StatusHistory.IN_PREPARATION;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static test.utils.OrderTestDataUtils.firstOrder;
 
 class OrderTest {
@@ -19,5 +24,21 @@ class OrderTest {
         BigDecimal actual = subject.getTotalAmount();
 
         assertEquals(expected.setScale(2, RoundingMode.HALF_UP), actual.setScale(2, RoundingMode.HALF_UP));
+    }
+
+    @Test
+    void cannot_update_order_if_not_create() {
+        RuntimeException error = assertThrows(RuntimeException.class, () -> subject.addDishOrders(List.of()));
+
+        assertEquals("Only CREATE status can be updated", error.getMessage());
+    }
+
+    @Test
+    void get_actual_status() {
+        OrderStatus expected = new OrderStatus("OS003", "O001", IN_PREPARATION, LocalDateTime.parse("2025-10-10T01:30:00"), LocalDateTime.parse("2025-10-10T01:30:00"));
+
+        OrderStatus actual = subject.getActualStatus();
+
+        assertEquals(expected, actual);
     }
 }
